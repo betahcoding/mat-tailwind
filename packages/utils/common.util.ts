dce6f754716f8062f18c78671ed9c869788ea88e
Betah Coding <betahcoding@gmail.com>
2024-05-07 15:42:34 +0700

    Move utils to package

---
+++ packages/utils/common.util.ts
@@ -0,0 +1,10 @@
+import fs from "fs";
+
+const getDirectories = (dirPath: string, excludes: string[] = []) => {
+  return fs
+    .readdirSync(dirPath, { withFileTypes: true })
+    .filter((dirent) => dirent.isDirectory() && !excludes.includes(dirent.name))
+    .map((folder) => folder.name);
+};
+
+export { getDirectories };
